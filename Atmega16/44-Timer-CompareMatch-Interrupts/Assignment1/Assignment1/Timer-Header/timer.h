58f3d249d939ecfe0c8fb48684242b041ac74f06
LNM31 <licanemes31@gmail.com>
2026-02-20 18:38:21 +0200

    cleaned code

--- Atmega16/44-Timer-CompareMatch-Interrupts/Assignment1/Assignment1/Timer-Header/timer.h
+++
@@ -1,7 +0,0 @@
-/*
- * timer.h
- *
- * Created: 10/08/2025 15:21:33
- *  Author: lican
- */ 
-void timer_init();
--- Atmega16/44-Timer-CompareMatch-Interrupts/Assignment3/Assignment2/Timer-Header/timer.h
+++
@@ -1,8 +0,0 @@
-/*
- * timer.h
- *
- * Created: 10/08/2025 15:21:33
- *  Author: lican
- */ 
-extern volatile int LEDF;
-void timer_init();
